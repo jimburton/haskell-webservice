be3eccd5f4455e33c03e14c57c35b87ffee65c07
James Burton <j.burton@brighton.ac.uk>
2022-04-23 11:01:45 +0100

    hlint

--- src/DB/Main.hs
+++ src/DB/Main.hs
@@ -1,20 +1,30 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import           Control.Applicative
+import Control.Applicative ()
 import qualified Data.Text as T
-import           Database.SQLite.Simple
-import           Database.SQLite.Simple.FromRow
-import           System.IO
-import           Data.Time.Format
-import           System.Random
-import           Data.List.Split
-import           Data.Char
-import           Data.Dates
+import Database.SQLite.Simple
+    ( close,
+      execute,
+      execute_,
+      open,
+      query_,
+      field,
+      FromRow(..),
+      ToRow(..), Connection )
+import Database.SQLite.Simple.FromRow ()
+import System.IO
+    ( hClose, openFile, hPutStrLn, Handle, IOMode(WriteMode) )
+import Data.Time.Format ()
+import System.Random ( getStdGen, Random(randomR), StdGen )
+import Data.List.Split ( splitOn )
+import Data.Char ( isSpace )
+import Data.Dates ( addInterval, DateInterval(Days), DateTime(..) )
+import Control.Monad (when)
 
 data WeatherField = WeatherField T.Text Float deriving (Show)
 
 instance FromRow WeatherField where
-  fromRow = WeatherField <$> field <*> field 
+  fromRow = WeatherField <$> field <*> field
 
 instance ToRow WeatherField where
   toRow (WeatherField theDate temp) = toRow (theDate, temp)
@@ -29,13 +39,15 @@ main = do
   mapM_ print r
   close conn
 
+populateDB :: Connection -> IO ()
 populateDB conn = do
-  ls <- fmap lines $ readFile "data/weather.txt"
+  ls <- lines <$> readFile "data/weather.txt"
   mapM_ (\l -> do let (x:xs) = splitOn "]" (tail l)
-                      num = (read (dropWhile isSpace (concat xs)))::Float
+                      num = read (dropWhile isSpace (concat xs))::Float
                   execute conn "INSERT INTO weather (the_date, temperature) VALUES (?,?)" (WeatherField (T.pack x) num)) ls
-        
 
+
+makeFile :: IO ()
 makeFile = do
   let nyd = DateTime {year=2017, month=1, day=1, hour=0, minute=0, second=0}
   h <- openFile "data/weather.txt" WriteMode
@@ -44,12 +56,12 @@ makeFile = do
   hClose h
 
 writeToFile :: DateTime -> Handle -> StdGen -> IO ()
-writeToFile d h g = if d <= DateTime {year=2017, month=12, day=31, hour=0, minute=0, second=0} 
-                    then do let (num, g') = randomR ((-40), 40) g
-                                date = showDateTime d
-                            hPutStrLn h ("[" ++  date ++ "] " ++ (show (num ::Float)))
-                            writeToFile (addInterval d (Days 1)) h g'
-                    else return ()
+writeToFile d h g = when (d <= DateTime {year=2017, month=12, day=31, hour=0, minute=0, second=0}) $
+  do let (num, g') = randomR (-40, 40) g
+         date = showDateTime d
+     hPutStrLn h ("[" ++  date ++ "] " ++ show (num ::Float))
+     writeToFile (addInterval d (Days 1)) h g'
+
 showDateTime :: DateTime -> String
-showDateTime (DateTime y m d _ _ _) = (pad y) ++ "-" ++ (pad m) ++ "-" ++ (pad d)
-  where pad d = if d < 10 then '0' : (show d) else show d
+showDateTime (DateTime y m d _ _ _) = pad y ++ "-" ++ pad m ++ "-" ++ pad d
+  where pad d = if d < 10 then '0' : show d else show d
